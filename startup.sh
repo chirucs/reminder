2f572b289fc0e9dfd073a887c21bee0f3d77c8cb
chiru <86065970+chirucs@users.noreply.github.com>
2025-07-02 22:55:10 -0400

    Update startup.sh

--- startup.sh
+++ startup.sh
@@ -1,5 +1,7 @@
-#!/bin/bash
-apt-get update
-apt-get install -y tesseract-ocr
-export PATH="/usr/bin:$PATH"
-streamlit run app.py --server.port 8000 --server.address 0.0.0.0 
+streamlit
+pytesseract
+Pillow
+pdf2image
+pdfminer.six
+opencv-python-headless
+easyocr
