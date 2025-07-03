d3599bab74343a8cbb970ddf6102e6f31ecb7e73
chiru <86065970+chirucs@users.noreply.github.com>
2025-07-02 22:53:48 -0400

    Update startup.sh

--- startup.sh
+++ startup.sh
@@ -2,4 +2,4 @@
 apt-get update
 apt-get install -y tesseract-ocr
 export PATH="/usr/bin:$PATH"
-streamlit run app.py --server.port 8000 --server.address 0.0.0.0
+streamlit run app.py --server.port 8000 --server.address 0.0.0.0 
